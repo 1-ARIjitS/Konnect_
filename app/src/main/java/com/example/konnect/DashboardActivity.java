12d8f660be049b8837b307c58cd2f8e0e4be3c7f
1-ARIjitS <arijits19@iiserb.ac.in>
2021-03-01 19:50:21 +0530

    logout button implemented on dashboard. empty meet code exception resolved with proper toasts. only bottom navigation bar left to implement.

--- app/src/main/java/com/example/konnect/DashboardActivity.java
+++ app/src/main/java/com/example/konnect/DashboardActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
@@ -19,59 +22,77 @@ import java.net.URL;
 public class DashboardActivity extends AppCompatActivity {
 
     EditText meet;
-    Button share,join;
+    Button share, join, logout;
+    String meetCode;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        meet=findViewById(R.id.codeBox);
-        share=findViewById(R.id.shareButton);
-        join=findViewById(R.id.joinButton);
+        meet = findViewById(R.id.codeBox);
+        share = findViewById(R.id.shareButton);
+        join = findViewById(R.id.joinButton);
+        logout = findViewById(R.id.logout_button);
+        meetCode = meet.getText().toString();
+        auth = FirebaseAuth.getInstance();
 
         URL serverUrl;
 
         try {
-         serverUrl=new URL("https://meet.jit.si");
+            serverUrl = new URL("https://meet.jit.si");
 
             JitsiMeetConferenceOptions defaultOptions = new JitsiMeetConferenceOptions.Builder()
-                            .setServerURL(serverUrl)
-                            .setWelcomePageEnabled(false)
-                            .build();
+                    .setServerURL(serverUrl)
+                    .setWelcomePageEnabled(false)
+                    .build();
             JitsiMeet.setDefaultConferenceOptions(defaultOptions);
-        }
-        catch (MalformedURLException e)
-        {
-           e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
 
 
         join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                JitsiMeetConferenceOptions options=new JitsiMeetConferenceOptions.Builder()
-                        .setRoom(meet.getText().toString())
-                        .setWelcomePageEnabled(false)
-                        .build();
 
-                JitsiMeetActivity.launch(DashboardActivity.this,options);
+                if (meetCode.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Invlaid,Blank Field please enter a valid meeting code", Toast.LENGTH_SHORT).show();
+                } else {
+                    JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                            .setRoom(meet.getText().toString())
+                            .setWelcomePageEnabled(false)
+                            .build();
+
+                    JitsiMeetActivity.launch(DashboardActivity.this, options);
+                }
             }
         });
 
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               String meetCode=meet.getText().toString();
 
-                Intent intent = new Intent(Intent.ACTION_SENDTO);
-                intent.setData(Uri.parse("mailto:"));
-                intent.putExtra(Intent.EXTRA_TEXT,"Here is the code to join the meeting"+" "+meetCode);
-                intent.putExtra(Intent.EXTRA_SUBJECT,"classified meeting code");
-                if (intent.resolveActivity(getPackageManager()) != null)
-                {
-                    startActivity(intent);
+                if (meetCode.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Invlaid,Blank Field please enter a valid meeting code", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO);
+                    intent.setData(Uri.parse("mailto:"));
+                    intent.putExtra(Intent.EXTRA_TEXT, "Here is the code to join the meeting" + " " + meetCode);
+                    intent.putExtra(Intent.EXTRA_SUBJECT, "classified meeting code");
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
                 }
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                finish();
             }
         });
     }
